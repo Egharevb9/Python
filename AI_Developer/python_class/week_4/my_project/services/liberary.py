65603a18c2c4e32f511f17630c01409aa8084f4e
esther <estherdominion2002@gmail.com>
2025-09-01 15:18:28 +0100

    trying projects out

--- AI_Developer/python_class/week_4/my_project/services/liberary.py
+++ AI_Developer/python_class/week_4/my_project/services/liberary.py
@@ -1,14 +1,14 @@
 # services/library.py - this will handle helper functions
-import my_data.data as data
+# import my_data.data as data
 
-def borrow_book(title):
-    for book in data.get_books():
-     if  book["title"].lower() == title.lower() and book["available"]:
-             book["available"] = False
-             return f"you have borrowed '{book['title']}'"
-     return  "Book not available"
+# def borrow_book(title):
+#     for book in data.get_books():
+#      if  book["title"].lower() == title.lower() and book["available"]:
+#              book["available"] = False
+#              return f"you have borrowed '{book['title']}'"
+#      return  "Book not available"
     
 
-def return_book(title):
-    for book in data.get_books():
-        if book["title"].lower()== title.lower() and book["avalable"]    
+# def return_book(title):
+#     for book in data.get_books():
+#         if book["title"].lower()== title.lower() and book["avalable"]    
